c02b9c4d059c7d0117cd2aa1b0e0819cddf615a7
M S I <M S I@MSI>
2024-03-15 21:26:05 +0700

    add gambar

--- lib/Form/header.form.dart
+++ lib/Form/header.form.dart
@@ -9,10 +9,18 @@ class HeaderForm extends StatelessWidget {
       children: [
         Padding(
           padding: const EdgeInsets.only(top: 20),
-          child: Text(
-            "masben Resto",
-            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          child: CircleAvatar(
+            radius: 50,
+            backgroundImage: NetworkImage(
+                'https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/A_small_cup_of_coffee.JPG/1200px-A_small_cup_of_coffee.JPG'),
           ),
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        Text(
+          "masben Resto",
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
         ),
         SizedBox(
           height: 20,
